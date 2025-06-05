80deb0259b2c34f972b0a22827e958c8cf09f6e3
Linn Amondi <linnamondi@gmail.com>
2025-06-05 13:42:28 +0300

    added three files index.html,index.js and style.css

---
+++ index.js
@@ -0,0 +1,29 @@
+
+document.body.style.backgroundColor="silver";
+document.getElementById("title").style.color="green"
+document.getElementById("subTitle").style.textTransform="uppercase"
+const newfruList=document.createElement("li");
+fruList.appendChild(newfruList);
+newfruList.textContent="Kiwi";
+const newvegList=document.createElement("li")
+vegList.appendChild(newvegList);
+newvegList.textContent="Cabbage"
+
+document.getElementById("title").style.marginLeft = "560px";
+document.getElementById("title").style.fontWeight="1500"
+document.getElementById("title").style.fontSize="45px"
+document.getElementById("paragraph").style.marginLeft="560px"
+document.getElementById("paragraph").style.fontSize="20px"
+document.getElementById("paragraph").style.fontWeight="900"
+
+document.getElementById("subTitle").style.marginLeft="560px"
+document.getElementById("fruList").style.marginLeft="530px"
+document.getElementById("subTitle").style.color="red"
+
+document.getElementById("subsubtitle").style.marginLeft="555px"
+document.getElementById("subsubtitle").style.textTransform="uppercase"
+document.getElementById("subsubtitle").style.color="red"
+document.getElementById("vegList").style.marginLeft="530px"
+
+
+
